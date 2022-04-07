e2afcc03d2865e37521e58d880e9eb050d8eeaa4
Mustafa Hesham <66361736+mustafa3370@users.noreply.github.com>
2022-04-08 01:55:25 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let loginform =document.querySelector('.header .login-form');
-
+let navbar =document.querySelector('.header  .navbar');
 document.querySelector('#login-btn').onclick = () =>{
     loginform.classList.toggle('active');
+    navbar.classList.remove('active');
 }
-let navbar =document.querySelector('.header .navbar');
-
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    loginform.classList.remove('active');
 }
+
+
+
+
